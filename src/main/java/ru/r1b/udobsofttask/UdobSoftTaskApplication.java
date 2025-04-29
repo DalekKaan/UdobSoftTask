ef851bd321b41efcee277c4e1bf6a9e966849408
Alexey Seleznev <rib@live.ru>
2025-04-29 16:39:20 +0300

    init

---
+++ src/main/java/ru/r1b/udobsofttask/UdobSoftTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.r1b.udobsofttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdobSoftTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdobSoftTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/r1b/udobsofttask/UdobSoftTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.r1b.udobsofttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdobSoftTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
